5fc433a758e0699d66bc2361d12496d3e7ad618f
webmamun25 <webmamun25@gmail.com>
2024-05-31 10:07:09 -0700

    Mongodb added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT | 5000
+app.use(cors())
+app.use(express.json())
+console.log(process.env.DB_USER)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdiymxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
