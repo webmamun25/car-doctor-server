f7d0b6d800719737dc44cdc7efbeddf56de4a011
webmamun25 <webmamun25@gmail.com>
2024-06-03 01:18:02 -0700

    full backend deploy correctly

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT | 5000
-app.use(cors())
+const port = process.env.PORT || 6000
+
 app.use(express.json())
-console.log(process.env.DB_USER)
-const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors({
+  origin: '*'
+ 
+}));
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdiymxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,17 +22,100 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
+
+    const ServicesCollection = client.db("CarDoctor").collection("services");
+    const BookingCollection = client.db("CarDoctor").collection("booking");
+    const UsersCollection = client.db("CarDoctor").collection("users");
+
+    app.get('/services', async(req, res) => {
+      const cursor = ServicesCollection.find();
+      const result=await cursor.toArray()
+      res.send(result)
       })
+    app.get('/users', async(req, res) => {
+      const cursor = UsersCollection.find();
+      const result=await cursor.toArray()
+      res.send(result)
+      })
+    app.get('/bookings', async(req, res) => {
+      
+      let query={}
+      if(req.query?.email){
+        query={email:req.query.email}
+      }
+      const result =await  BookingCollection.find(query).toArray();
+      res.send(result)
+      })
+    app.get('/checkout/:id', async(req, res) => {
+      const id=req.params.id 
+      const query={_id:new ObjectId(id)}
+    
+      const result=await ServicesCollection.findOne(query)
+      res.send(result)
+      })
+      
+    app.post('/bookings',async(req,res)=>{
+      const bookings=req.body
+      const result =await BookingCollection.insertOne(bookings)
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result)
+    })
+    app.post('/users',async(req,res)=>{
+      const users=req.body
+      console.log(users)
+      const result =await UsersCollection.insertOne(users)
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result)
+    })
+
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await BookingCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.patch('/bookings/:id',async(req,res)=>{
+      const id=req.params.id
+      const filter={_id:new ObjectId(id)}
+     
+      const updateBookings=req.body 
+      const updatedDOc={
+        $set:{
+       
+          status:updateBookings.status
+        }
+      }
+      const result=await BookingCollection.updateOne(filter,updatedDOc)
+      res.send(result)
+
+    })
+    app.patch('/users/:email',async(req,res)=>{
+      const email=req.params.email
+      const filter={email:email}
+     console.log(filter)
+      const updatedUsers=req.body 
+      const updatedDOc={
+        $set:{
+       
+          role:updatedUsers.role
+        }
+      }
+      const result=await UsersCollection.updateOne(filter,updatedDOc)
+      console.log(result)
+
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
